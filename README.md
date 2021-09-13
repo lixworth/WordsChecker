4a296d5aa2d456b8d2af07aca956d293d1769228
lixworth <lixworth@gmail.com>
2021-09-13 21:54:27 +0800

    初步完成

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # WordsChecker
 敏感词检测器
 
-# How to use
+## How to use
 - git clone git@github.com:CubeSofa/WordsChecker.git
 - yarn install
 - yarn run server
+
+## HTTP API
+自己看代码吧 （ 没得时间写文档
--- main.js
+++ main.js
@@ -12,22 +12,106 @@ const Pinyin = require('./ToolGood.Words.Pinyin.min');
 const Translate = require('./ToolGood.Words.Translate.min');
 const StringSearch = require('./ToolGood.Words.StringSearch.min');
 const WordsSearch = require('./ToolGood.Words.WordsSearch.min');
+const fs = require("fs");
 
 let pinyin = new Pinyin();
 let translate = new Translate();
-let search = new StringSearch();
+let search = new WordsSearch();
+const s = fs.readFileSync('./ToolGood.Words/csharp/ToolGood.Words.Contrast/BadWord.txt','utf-8')
+search.SetKeywords(s.split(/[(\r\n)\r\n]+/));
 
 function response(success = false,message = "拉取成功",data = []) {
     return {
         success: success,
         message: message,
-        data: data
+        // data: data
     };
 }
-app.get('/',(req,res) => {
-    res.send(response(true,pinyin.GetPinyin("我爱中国")))
+app.get('/pinyin/get',(req,res) => {
+    let query_data = req.query.data;
+    let yin_jie = Boolean(req.query.yin_jie);
+    if(query_data){
+        res.send(response(true,pinyin.GetPinyin(req.query.data,yin_jie)));
+    }else{
+        res.send(response(false,"缺少参数"));
+    }
+});
+app.get('/pinyin/first',(req,res) => {
+    let query_data = req.query.data;
+    if(query_data){
+        res.send(response(true,pinyin.GetFirstPinyin(req.query.data)));
+    }else{
+        res.send(response(false,"缺少参数"));
+    }
+});
+app.get('/pinyin/all',(req,res) => {
+    let query_data = req.query.data;
+    if(query_data){
+        res.send(response(true,pinyin.GetAllPinyin(req.query.data)));
+    }else{
+        res.send(response(false,"缺少参数"));
+    }
+});
+app.get('/pinyin/name',(req,res) => {
+    let query_data = req.query.data;
+    let yin_jie = Boolean(req.query.yin_jie);
+    if(query_data){
+        res.send(response(true,pinyin.GetPinyinForName(req.query.data,yin_jie)));
+    }else{
+        res.send(response(false,"缺少参数"));
+    }
+});
+
+app.get('/translate/to_simplified',(req,res) => {
+    let query_data = req.query.data;
+    let type = req.query.type;
+    if(!type){
+        type = 0;
+    }
+    if(query_data){
+        res.send(response(true,translate.ToSimplifiedChinese(req.query.data,type)));
+    }else{
+        res.send(response(false,"缺少参数"));
+    }
+});
+app.get('/translate/to_traditional',(req,res) => {
+    let query_data = req.query.data;
+    let type = req.query.type;
+    if(!type){
+        type = 0;
+    }
+    if(query_data){
+        res.send(response(true,translate.ToTraditionalChinese(req.query.data,type)));
+    }else{
+        res.send(response(false,"缺少参数"));
+    }
+});
+
+app.get('/search/replace',(req,res) => {
+    let query_data = req.query.data;
+    if(query_data){
+        res.send(response(true,search.Replace(req.query.data,'*')));
+    }else{
+        res.send(response(false,"缺少参数"));
+    }
+});
+app.get('/search/all',(req,res) => {
+    let query_data = req.query.data;
+    if(query_data){
+        res.send(response(true,search.ContainsAny(req.query.data)));
+    }else{
+        res.send(response(false,"缺少参数"));
+    }
+});
+app.get('/search/check',(req,res) => {
+    let query_data = req.query.data;
+    if(query_data){
+        res.send(response(true,search.ContainsAny(req.query.data)));
+    }else{
+        res.send(response(false,"缺少参数"));
+    }
 });
 
 app.listen(port,()=>{
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`WordsChecker app listening at http://localhost:${port}`)
 });
